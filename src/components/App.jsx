c73105211b3606f74c2744237b4179deb0ae043f
arkteck <arkteck@gmail.com>
2022-04-25 20:20:08 -0400

    Progress after first day.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,61 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+import searchYouTube from '../lib/searchYouTube.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoList: exampleVideoData,
+      nowPlaying: exampleVideoData[0],
+      query: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.searchCallback = this.searchCallback.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleClick(video) {
+    this.setState({nowPlaying: video});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    searchYouTube(this.state.query, this.searchCallback);
+  }
+  searchCallback(data) {
+    this.setState({videoList: data, nowPlaying: data[0]});
+  }
+  handleChange(event) {
+    this.setState({query: event.target.value});
+    searchYouTube(this.state.query, this.searchCallback);
+  }
+  componentDidMount() {
+    // this.setState({videoList: exampleVideoData, nowPlaying: exampleVideoData[0]});
+    searchYouTube('cats', this.searchCallback);
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7" id="videoPlayer">
+            <VideoPlayer video={this.state.nowPlaying} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videoList} handleClick={this.handleClick} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -4,8 +4,18 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
-var searchYouTube = (query, callback) => {
+var searchYouTube = (query = 'cats', callback) => {
   // TODO
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: { q: query, youtube_api_key: YOUTUBE_API_KEY},
+    contentType: 'application/json',
+    success: callback,
+    error: function(error) {
+      console.error('YouTube: Failed to fetch videos', error);
+    }
+  });
 };
 
 export default searchYouTube;
